3522bc807f8b219d5bfbee2cbf00505c7bd3f721
marinaramirofde <marinaramirofde@gmail.com>
2023-03-09 16:16:19 +0100

    Feature #12: Varios marcadores en el mapa y sus popups (datos ejemplo)

--- src/components/ResourcesListEdit.js
+++ src/components/ResourcesListEdit.js
@@ -51,6 +51,16 @@ export default function ResourcesList() {
     //className: 'leaflet-div-icon'
   });
 
+  const markers = [
+    { position: [37.37819890742758, -5.986237173442225], title: "Marcador 1"},
+    { position: [37.36389157436658, -5.98052205673053], title: "Marcador 2" },
+    { position: [37.38551392139047, -5.972622733550851], title: "Marcador 3" },
+    { position: [37.39455877755949, -5.989694735085925], title: "Marcador 4" },
+    { position: [37.357317735050124, -5.984544892507049], title: "Marcador 5" },
+    { position: [37.3652312981789, -5.992698807621536], title: "Marcador 6" },
+    { position: [37.35124555623236, -5.989008088148664], title: "Marcador 7" }
+  ];
+  
   return (
     <div>
       
@@ -75,9 +85,13 @@ export default function ResourcesList() {
                             "https://api.maptiler.com/maps/streets-v2/256/{z}/{x}/{y}.png?key=4Qg1CBLvuefoRWUOrqlJ"
                           }
                         />
-                        <Marker icon={customIcon} position={[37.358342303352885, -5.986570537333228]}>
+                        {/* <Marker icon={customIcon} position={[37.358342303352885, -5.986570537333228]}>
                           <Popup>Marcador</Popup>
-                        </Marker>
+                        </Marker> */}
+                         {markers.map((marker, index) => (
+                          <Marker key={index} icon={customIcon} position={marker.position}> <Popup>{marker.title}</Popup>
+                          </Marker>
+                          ))}
                       </MapContainer>
                     </MDBRow>
                   </MDBCol>
