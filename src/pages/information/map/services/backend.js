5bfad39c0d915aff3adc6ccb800b78dad6df3e6a
marinaramirofde <marinaramirofde@gmail.com>
2023-03-12 15:31:07 +0100

    Fix #12: codacy

--- src/pages/information/map/services/backend.js
+++ src/pages/information/map/services/backend.js
@@ -4,6 +4,6 @@ import { backendUrl } from "../../../../config";
 const resources = axios.create({
     baseURL: String(backendUrl + "information/resources/"),
     timeout: 1000
-  });
+});
 
 export default resources
