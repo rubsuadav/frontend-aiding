c7a6bc6ba9035ae0aa2a12cbc8f265ec7a384067
marolmmar1 <marolmmar1@alum.us.es>
2023-03-17 11:47:04 +0100

    feature #9: formatted Certificate, still haaving trouble with tables

--- src/pages/partners/Certificate.js
+++ src/pages/partners/Certificate.js
@@ -11,17 +11,76 @@ import {
 } from "mdb-react-ui-kit";
 import Communication from "./Communications.js";
 import {partners, fileUrl} from "./services/backend.js";
-import { Document, Page, PDFViewer, Text } from "@react-pdf/renderer";
+import { Document, Page, PDFViewer, Text, Image, StyleSheet} from "@react-pdf/renderer";
 
-function generateCertificate(user, language){
-  
+function generateCertificate(user, language, periodicity){
+  const styles = StyleSheet.create({
+    body:{
+      paddingTop: 60,
+      paddingBotom: 60,
+      paddingHorizontal: 60,
+      position: "absolute"
+    },
+    image:{
+      height:70,
+      width: 180
+    },
+    footer:{
+      position: "absolute",
+      bottom: "30",
+      textAlign: "center",
+      fontSize: 10,
+      fontFamily:"Courier",
+      alignSelf: "center"
+    },
+    stamp:{
+      height:120,
+      width: 210,
+      alignSelf: "center"
+    },
+    text:{
+      fontSize: 10,
+      paddingHorizontal: 10,
+      paddingVertical: 10
+    },
+    textSp:{
+      fontSize: 10,
+      paddingHorizontal: 10,
+      paddingVertical: 20,
+      alignSelf: "center",
+      fontFamily:"Helvetica-Bold"
+      
+    },
+    textStp:{
+      fontSize: 10,
+      paddingVertical: 20,
+      alignSelf: "center"
+    }
+
+
+  });
   if(language == "Español")
     return(
       <Document>
-          <Page>
-            <Text>español</Text>
-            <Text>{user.name}</Text>
-            <Text>{user.dni}</Text>
+          <Page style={styles.body}>
+            <Image src="http://localhost:3000/boscoglobal-logo.png" height="50" style={styles.image}></Image>
+            <Text style={styles.text}>Francisco Antonio Echeto Zubiri con DNI 15.801.110 H, Tesorero de la Fundación Bosco Global, con domicilio social en Sevilla, c/ Salesianos 3 A y CIF G-90.196.072</Text>
+            <Text style={styles.textSp}>DECLARA</Text>
+            <Text style={styles.text}>Que la <Text style={styles.textSp}>Fundación Bosco Global</Text> se encuentra incluida entre las acogidas al régimen de deducciones establecido por la Ley 49/2002, de 23 de diciembre de régimen fiscal de las entidades sin fines lucrativos y de los incentivos fiscales al mecenazgo.</Text>
+            <Text style={styles.text}>Que se ha recibido de:</Text>
+            <Text>Nombre: {user.name}</Text>
+            <Text>Apellido: {user.last_name}</Text>
+            <Text>NIF: {user.dni}</Text>
+            <Text>Provincia: {user.province}</Text>
+            <Text>Perioicidad: {periodicity}</Text>
+            <Text style={styles.text}>Que esta cantidad ha sido entregada con carácter de donación pura, simple e irrevocable.</Text>
+            <Text style={styles.text}>Que el donativo recibido se ha destinado para el apoyo a los proyectos que la Entidad desarrolla para el cumplimiento de sus fines.</Text>
+            <Text style={styles.text}>Sirva el presente documento para que surta los efectos fiscales oportunos.</Text>
+            <Text style={styles.textStp}>Sevilla, abril de 2023</Text>
+            <Image src="http://localhost:3000/Bosco-sello.png" height="50" style={styles.stamp}></Image>
+            <Text style={styles.textStp}>Francisco Antonio Echeto Zubiri</Text>
+            <Text style={styles.textStp}>Tesorero de la Fundación Bosco Global</Text>
+            <Text style={styles.footer}>C/Salesianos,3A. 41008 Sevilla (España) · T. 954 53 28 27 · info@boscoglobal.org · boscoglobal.org</Text>
           </Page>
       </Document>
     )
@@ -106,8 +165,8 @@ export default function Certificate() {
     <div>
       <button onClick={generateCertificate(user, "Catalán")}>Catalán</button>
     </div>
-      <PDFViewer>
-        {generateCertificate(user,"Español")}         
+      <PDFViewer height={"450"}>
+        {generateCertificate(user,"Español", donation.periodicity)}         
       </PDFViewer>
     </section>
   );
