6bdffb753a1f9c5bea6baca44c64e0de482170f6
marinaramirofde <marinaramirofde@gmail.com>
2023-04-13 19:03:34 +0200

    Feature #59: Construcción del botón de crear un twit con texto automático

--- src/components/SocialMedia.js
+++ src/components/SocialMedia.js
@@ -1,19 +1,26 @@
+import React from 'react';
 import Twit from 'twit';
 
-const twit = new Twit({
-  consumer_key: 'TU_CONSUMER_KEY',
-  consumer_secret: 'TU_CONSUMER_SECRET',
-  access_token: 'TU_ACCESS_TOKEN',
-  access_token_secret: 'TU_ACCESS_TOKEN_SECRET'
-});
+export default function TweetButton() {
 
-
-const handleClick = () => {
-  const tweetText = 'Estoy usando Aiding, este es su correo: aiding@gmail.com';
-
-  const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(
-    tweetText
-  )}`;
-
-  window.open(tweetUrl, '_blank');
-};
+    const handleClick = () => {
+      const tweetText = 'Estoy usando Aiding!';
+      const twit = new Twit({
+        consumer_key: 'TU_CONSUMER_KEY',
+        consumer_secret: 'TU_CONSUMER_SECRET',
+        access_token: 'TU_ACCESS_TOKEN',
+        access_token_secret: 'TU_ACCESS_TOKEN_SECRET'
+      });
+      twit.post('statuses/update', { status: tweetText })
+        .then(response => console.log('Tweet publicado:', response.data))
+        .catch(error => console.log('Error al publicar el tweet:', error));
+  
+      const tweetUrl = `https://twitter.com/intent/tweet?text=${encodeURIComponent(tweetText)}`;
+      window.open(tweetUrl, '_blank');
+    };
+  
+    return (
+      <button onClick={handleClick}>Publicar Tweet</button>
+    );
+  }
+  
